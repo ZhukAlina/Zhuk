e62f43decad1bf1f648499bd6576a5d9dc6c85c8
Alina <alya.griboed@gmail.com>
2022-10-24 08:52:51 +0300

    3d massiv

---
+++ 333.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int n = 3;
+	int*** array = new int**[n];
+	for(int i = 0; i<n;i++){
+		array[i] = new int*[n];
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			array[i][j] = new int[n];
+		}
+	}
+	
+	int count = 1;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			for (int k = 0; k < n; k++)
+			{
+				array[i][j][k] = count;
+				count++;
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			for (int k = 0; k < n; k++)
+			{
+				cout << array[i][j][k] << ' ';
+			}
+			cout << endl;
+		}
+		cout << endl;
+	}
+
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			delete[] array[i][j];
+		}
+	}
+	return 0;
+}
